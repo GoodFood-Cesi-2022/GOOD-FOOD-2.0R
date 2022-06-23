bf3d425761511c6bd7e1733cf0fb46fdf78826ff
cuisinier05 <alexandre.riera.simplon@gmail.com>
2022-06-23 22:40:52 +0200

    Docker is functional ! All fixed

--- src/Config.ts
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'react-native';
-
-// Address to stripe server running on local machine
-export const LOCAL_URL =
-  Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
-
-export const API_URL = 'https://rigorous-heartbreaking-cephalopod.glitch.me';
